7ba5edb9ad1b538f3bcae993c7f6dc3ca98e6772
Kumar Nitesh <kumar.nitesh@endurance.com>
2019-11-20 08:50:19 -0500

    Working Website selection

--- index.js
+++ index.js
@@ -7,26 +7,21 @@ const express = require('express')
 const path = require('path')
 const basicAuth = require('express-basic-auth')
 const bodyParser = require('body-parser')
-const { WebhookClient,Card, Suggestion } = require('dialogflow-fulfillment')
+const { WebhookClient} = require('dialogflow-fulfillment')
 
 
 
 // Import the service function and various response classes
 const {
-  dialogflow,
-  actionssdk,
-  Image,
-  Table,
-  Carousel,
-  Permission
+  dialogflow
 } = require('actions-on-google');
 
 
 
-const {welcomeIntent} = require('./server/intents/welcome')
-const {PurchaseDomainIntent} = require('./server/intents/purchase-domain')
-const {fallbackIntent, DisplayOptions} = require('./server/intents/fallback')
-const {BuildAWebsiteStep1Intent,BuildAWebsiteStep2Intent} = require('./server/intents/build-website')
+const {welcomeIntent, welcomeSmsIntent} = require('./server/intents/welcome')
+const {PurchaseDomainIntent, PurchaseDomainSmsIntent} = require('./server/intents/purchase-domain')
+const {fallbackIntent,fallbackSmsIntent, DisplayOptions} = require('./server/intents/fallback')
+const {CreateWebsiteIntent,CreateWebsiteStep3Intent, CreateWebsiteStep2Intent } = require('./server/intents/build-website')
 const port = process.env.PORT || 3000;
 
 const expressApp = express().use(bodyParser.json());
@@ -37,8 +32,9 @@ const dialogflowApp = dialogflow();
 dialogflowApp.intent('Default Welcome Intent', (conv)=> welcomeIntent(conv));
 
 dialogflowApp.intent('Default Fallback Intent', (conv)=> fallbackIntent(conv)); 
-dialogflowApp.intent('Create Website', (conv)=> BuildAWebsiteStep1Intent(conv));
-dialogflowApp.intent('BuildaWebsite - build website-Step2', (conv)=> BuildAWebsiteStep2Intent(conv));
+dialogflowApp.intent('Create Website', (conv)=> CreateWebsiteIntent(conv));
+dialogflowApp.intent('Create Website - Step2 - Start Creating New WebPage', (conv,param)=> CreateWebsiteStep2Intent(conv,param));
+dialogflowApp.intent('Create Website - Step3 - Start Creating New WebPage', (conv,params, option)=> CreateWebsiteStep3Intent(conv,params, option));
 dialogflowApp.intent('Purchase Domain', (conv,param)=> PurchaseDomainIntent(conv,param));
 dialogflowApp.intent('Show Display Options',(conv) => DisplayOptions(conv))
 
@@ -62,4 +58,47 @@ expressApp.get('/', function (req, res) {
 expressApp.get('/status', (req, res) => res.send('Up and running'))
 
 expressApp.post('/webai', dialogflowApp)
+
+// expressApp.post('/webaisms',(req, res) => {
+  
+//   const agent = new WebhookClient({ request: req, response: res })
+
+//   console.log("\n\n************************************************\n")
+//   // console.log('Dialogflow Request headers: \n' + JSON.stringify(req.headers));
+//   console.log("\n\n************************************************\n")
+//   // console.log('Dialogflow Request body: \n' + JSON.stringify(req.body));
+//   console.log('queryText: \n' + JSON.stringify(req.body.queryResult.queryText));
+//   console.log('fulfillmentText: \n' + JSON.stringify(req.body.queryResult.fulfillmentText));
+//   console.log('fulfillmentText: \n' + JSON.stringify(req.body.queryResult.parameters));
+//   console.log("\n\n************************************************")
+ 
+
+//   const welcomeHandler = () => welcomeSmsIntent(agent)
+
+//   const fallbackHandler = () => fallbackSmsIntent(agent)
+
+//   const BuildAWebsiteStep1Handler = () =>BuildAWebsiteStep1SmsIntent(agent)
+
+//   const BuildAWebsiteStep2Handler = () =>BuildAWebsiteStep2SmsIntent(agent)
+
+//   const PurchaseDomainHandler = () => PurchaseDomainSmsIntent(agent,req.body.queryResult)
+
+//   // const BuildAWebsiteStep2Handler = () =>BuildAWebsiteStep2Intent(agent)
+
+//   let intentMap = new Map()
+
+//   intentMap.set('Default Welcome Intent', welcomeHandler)
+//   intentMap.set('Default Fallback Intent', fallbackHandler); 
+//   intentMap.set('Create Website', BuildAWebsiteStep1Handler);
+//   intentMap.set('BuildAWebsite - build website-Step2', BuildAWebsiteStep2Handler);
+//   intentMap.set('Purchase Domain', PurchaseDomainHandler);
+
+
+//   try{
+//     agent.handleRequest(intentMap)
+//   }catch(err){
+//     console.log(err)
+//   }
+  
+// }) 
 
--- server/intents/build-website.js
+++ server/intents/build-website.js
@@ -1,20 +1,131 @@
-const {  Suggestions, Image, } = require('actions-on-google');
+const {  Suggestions, Image,BasicCard, Button, Carousel } = require('actions-on-google');
 
 
-const createWebsiteStep1 =  (conv) =>{
+const CreateWebsiteIntent =  (conv) =>{
   conv.ask(`Great, lets build a website. But before that I will need few basic information. Let me know when you are ready.`);   
   conv.ask(new Suggestions(`Ok`));
   conv.ask(new Suggestions(`Ready`));
   conv.ask(new Suggestions(`Let's do it.`));
 }
 
-const createWebsiteStep2 =  (conv) => {
+const CreateWebsiteStep1Intent =  (conv) => {
   conv.ask(`Hold Tight, we are working on creating a new URL for your awesome website.`);
-  conv.ask( new Image('https://cloud.peachd.com/prod/images/gateway_v2/intro/menus.png'))
  
 }
+
+const CreateWebsiteStep2Intent =  (conv,param) => {
+  // console.log(param)
+  conv.ask('Select a theme for your website');
+  // Create a carousel
+  conv.ask(new Carousel({
+    title: 'Website Theme',
+    items: {
+      // Add the first item to the carousel
+      'SELECTION_KEY_THEME_ONE': {
+        synonyms: [
+          'Theme 1',
+          'Theme one',
+          'Theme A',
+        ],
+        title: 'Theme 1',
+        description: 'This is a description of a carousel item.',
+        image: new Image({
+          url: 'https://storage.googleapis.com/actionsresources/logo_assistant_2x_64dp.png',
+          alt: 'Image alternate text',
+        }),
+      },
+      // Add the second item to the carousel
+      'SELECTION_KEY_THEME_TWO': {
+        synonyms: [
+          'Theme 2',
+          'Theme two',
+          'Theme B',
+      ],
+        title: 'Theme 2',
+        description: 'Google Home is a voice-activated speaker powered by ' +
+          'the Google Assistant.',
+        image: new Image({
+          url: 'https://storage.googleapis.com/actionsresources/logo_assistant_2x_64dp.png',
+          alt: 'Google Home',
+        }),
+      },
+      // Add the third item to the carousel
+      'SELECTION_KEY_THEME_THREE': {
+        synonyms: [
+          'Theme 3',
+          'Theme three',
+          'Theme C',
+        ],
+        title: 'Theme 3',
+        description: 'Pixel. Phone by Google.',
+        image: new Image({
+          url: 'https://storage.googleapis.com/actionsresources/logo_assistant_2x_64dp.png',
+          alt: 'Google Pixel',
+        }),
+      },
+    },
+  }));
+
+  conv.contexts.set('theme-selected', 5, param);
+  // const {websiteName,websiteCategory,websiteDescription} = param
+  // const websiteURL = websiteName.replace(' ','_')+'.constantcontact.online'
+  // conv.ask(`Here's your awesome website`);
+  // conv.ask(new BasicCard({
+  //   text: websiteDescription, // Note the two spaces before '\n' required for
+  //                                // a line break to be rendered in the card.
+  //   subtitle: websiteCategory,
+  //   title: websiteName,
+  //   buttons: new Button({
+  //     title: 'View your website',
+  //     url: `https://${websiteURL}/`,
+  //   }),
+  //   image: new Image({
+  //     url: 'https://storage.googleapis.com/actionsresources/logo_assistant_2x_64dp.png',
+  //     alt: websiteName,
+  //   }),
+  //   display: 'CROPPED',
+  // }));
+  // conv.ask('What you want to do next?');
+  // conv.ask(new Suggestions(`Purchase Domain`));
+  // conv.ask(new Suggestions(`Get listed on Google`));
+ 
+}
+
+const CreateWebsiteStep3Intent = (conv,params, option) =>{
+
+  // const {websiteName,websiteCategory,websiteDescription} = conv.contexts.get('theme-selected')
+   console.log(params, option)
+    const SELECTED_ITEM_RESPONSES = {
+      'SELECTION_KEY_THEME_ONE': 'selected Theme 1',
+      'SELECTION_KEY_THEME_TWO': 'selected Theme 2',
+      'SELECTION_KEY_THEME_THREE': 'saelected Theme 3',
+    };
+
+  //  conv.ask(SELECTED_ITEM_RESPONSES[option]);
+  const {websiteName,websiteCategory,websiteDescription} = conv.contexts.get('theme-selected').parameters
+  const websiteURL = websiteName.replace(' ','_')+'.constantcontact.online'
+  conv.ask(`Here's your awesome website with `+ (option && SELECTED_ITEM_RESPONSES[option]) || (params && SELECTED_ITEM_RESPONSES[params.selectedTheme]));
+  conv.ask(new BasicCard({
+    text: websiteDescription, // Note the two spaces before '\n' required for
+                                 // a line break to be rendered in the card.
+    subtitle: websiteCategory,
+    title: websiteName,
+    buttons: new Button({
+      title: 'View your website',
+      url: `https://${websiteURL}/`,
+    }),
+    image: new Image({
+      url: 'https://storage.googleapis.com/actionsresources/logo_assistant_2x_64dp.png',
+      alt: websiteName,
+    }),
+    display: 'CROPPED',
+  }));
+  conv.ask('What you want to do next?');
+  conv.ask(new Suggestions(`Purchase Domain`));
+  conv.ask(new Suggestions(`Get listed on Google`));
+  
+}
 module.exports = {
-    BuildAWebsiteStep1Intent : createWebsiteStep1,
-  BuildAWebsiteStep2Intent : createWebsiteStep2
+  CreateWebsiteIntent,CreateWebsiteStep1Intent, CreateWebsiteStep2Intent ,CreateWebsiteStep3Intent
 }
 
